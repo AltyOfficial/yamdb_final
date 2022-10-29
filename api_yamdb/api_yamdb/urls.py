2c1e6f701cc1f9862edce15829955655a487a6cb
Arthur Alty <artouralty.jesus@gmail.com>
2022-10-29 21:56:28 +0300

    test

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-
-def hello(request):
-    return HttpResponse('Привет Катя')
-
-
 urlpatterns = [
-    path('hello/', hello),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(
-        'redoc/',
+        'redo/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
