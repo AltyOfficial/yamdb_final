e51f157c32e10c1365c04267a624911448114432
Arthur Alty <artouralty.jesus@gmail.com>
2022-10-29 21:45:13 +0300

    test

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
 from django.db.models import Avg
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
